5efe5998fb02d908a8238f5e727f9575fcd1f613
Greg Delgado <grd888@gmail.com>
2025-04-13 12:28:18 +0800

    extract markdown images

--- src/test_utils.py
+++ src/test_utils.py
@@ -2,7 +2,7 @@ import unittest
 
 from textnode import TextNode, TextType
 
-from utils import text_node_to_html_node
+from utils import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images
 
 class TestUtils(unittest.TestCase):
     def test_text(self):
@@ -10,6 +10,143 @@ class TestUtils(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_split_nodes_delimiter_code(self):
+        # Test for code blocks with backtick delimiter
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code block")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+    
+    def test_split_nodes_delimiter_bold(self):
+        # Test for bold text with ** delimiter
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "bold")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+    
+    def test_split_nodes_delimiter_italic(self):
+        # Test for italic text with _ delimiter
+        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with an ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "italic")
+        self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+    
+    def test_split_nodes_delimiter_multiple_occurrences(self):
+        # Test for multiple occurrences of the same delimiter
+        node = TextNode("This `code` has `multiple` code blocks", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[0].text, "This ")
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " has ")
+        self.assertEqual(new_nodes[3].text, "multiple")
+        self.assertEqual(new_nodes[3].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[4].text, " code blocks")
+    
+    def test_split_nodes_delimiter_no_delimiter(self):
+        # Test when no delimiter is found
+        node = TextNode("This text has no delimiters", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0].text, "This text has no delimiters")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+    
+    def test_split_nodes_delimiter_incomplete_pair(self):
+        # Test when there's an incomplete pair of delimiters
+        node = TextNode("This text has an incomplete ` delimiter", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        
+        # The current implementation splits on the delimiter, resulting in 2 parts
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual(new_nodes[0].text, "This text has an incomplete ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, " delimiter")
+        self.assertEqual(new_nodes[1].text_type, TextType.TEXT)
+    
+    def test_split_nodes_delimiter_empty_content(self):
+        # Test with empty content between delimiters
+        node = TextNode("This has an empty `` code block", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual(new_nodes[0].text, "This has an empty ")
+        self.assertEqual(new_nodes[1].text, " code block")
+    
+    def test_split_nodes_delimiter_multiple_nodes(self):
+        # Test with multiple input nodes
+        node1 = TextNode("This is `code`", TextType.TEXT)
+        node2 = TextNode("This is normal text", TextType.TEXT)
+        node3 = TextNode("This is more `code`", TextType.TEXT)
+        
+        new_nodes = split_nodes_delimiter([node1, node2, node3], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, "This is normal text")
+        self.assertEqual(new_nodes[3].text, "This is more ")
+        self.assertEqual(new_nodes[4].text, "code")
+        self.assertEqual(new_nodes[4].text_type, TextType.CODE)
+    
+    def test_split_nodes_delimiter_non_text_nodes(self):
+        # Test with non-TEXT type nodes
+        node1 = TextNode("This is `code`", TextType.TEXT)
+        node2 = TextNode("This is bold", TextType.BOLD)
+        
+        new_nodes = split_nodes_delimiter([node1, node2], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, "This is bold")
+        self.assertEqual(new_nodes[2].text_type, TextType.BOLD)
+    
+    def test_extract_markdown_images(self):
+        # Test with multiple images
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        images = extract_markdown_images(text)
+        
+        self.assertEqual(len(images), 2)
+        self.assertEqual(images[0], ("rick roll", "https://i.imgur.com/aKaOqIh.gif"))
+        self.assertEqual(images[1], ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"))
+    
+    def test_extract_markdown_images_no_images(self):
+        # Test with no images
+        text = "This is text with no markdown images"
+        images = extract_markdown_images(text)
+        
+        self.assertEqual(len(images), 0)
+    
+    def test_extract_markdown_images_incomplete_syntax(self):
+        # Test with incomplete markdown image syntax
+        text = "This has an incomplete ![alt text](https://example.com and ![another](no closing parenthesis"
+        images = extract_markdown_images(text)
+        
+        self.assertEqual(len(images), 0)
   
 if __name__ == "__main__":
     unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 from htmlnode import LeafNode
 
@@ -12,3 +13,70 @@ def text_node_to_html_node(text_node):
     return LeafNode("a", text_node.text, {"href": text_node.url})
   else:
     raise ValueError(f"Unknown text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+  result = []
+  
+  for old_node in old_nodes:
+    # Only process TextNode with TEXT type
+    if not isinstance(old_node, TextNode) or old_node.text_type != TextType.TEXT:
+      result.append(old_node)
+      continue
+    
+    # Split the text by delimiter
+    splits = old_node.text.split(delimiter)
+    
+    # If no delimiter found, keep the node as is
+    if len(splits) <= 1:
+      result.append(old_node)
+      continue
+      
+    # Check if we have an even number of splits (complete delimiter pairs)
+    # If odd number of splits, we have incomplete delimiter pairs
+    has_complete_pairs = len(splits) % 2 == 1
+    
+    # Process text with delimiters
+    for i in range(len(splits)):
+      # Skip empty splits
+      if not splits[i]:
+        continue
+      
+      # For complete pairs: even indices are TEXT, odd indices are special text_type
+      # For incomplete pairs: only properly enclosed text gets the special text_type
+      if has_complete_pairs:
+        if i % 2 == 0:
+          result.append(TextNode(splits[i], TextType.TEXT))
+        else:
+          result.append(TextNode(splits[i], text_type))
+      else:
+        # For incomplete pairs, only text between complete delimiter pairs gets the special type
+        # This means odd indices less than the last index get the special type
+        if i % 2 == 1 and i < len(splits) - 1:
+          result.append(TextNode(splits[i], text_type))
+        else:
+          result.append(TextNode(splits[i], TextType.TEXT))
+  
+  return result
+
+def extract_markdown_images(text):
+  """Extract markdown images from text and return a list of tuples with alt text and URL.
+  
+  Args:
+    text: A string containing markdown text
+    
+  Returns:
+    A list of tuples, each containing (alt_text, url) for each markdown image found
+  
+  Example:
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+    extract_markdown_images(text) # [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
+  """
+  # Regular expression pattern for markdown images: ![alt text](url)
+  # The pattern captures two groups: the alt text and the URL
+  pattern = r"!\[(.*?)\]\((.*?)\)"
+  
+  # Find all matches in the text
+  matches = re.findall(pattern, text)
+  
+  # Each match is a tuple of (alt_text, url)
+  return matches
